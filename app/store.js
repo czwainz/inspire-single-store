256c90919eb5b3a313d7acadb928fccce870364a
Chrissy Zwainz <zchrissychrissy@gmail.com>
2019-02-19 09:12:27 -0700

    udpated store

--- app/store.js
+++ app/store.js
@@ -1,7 +1,7 @@
 //PRIVATE
-//IMAGES
 
-//QUOTES 
+let store;
+
 let _state = {
   images: {},
   quotes: {},
@@ -9,74 +9,30 @@ let _state = {
   weather: {},
 }
 
-let _subscribers = []
+let _subscribers = {
+  images: {},
+  quotes: {},
+  todos: [],
+  weather: {},
+}
 
 
-//TODO
-
-//WEATHER
 
 
 //PUBLIC
 export default class Store {
-  //IMAGES
-
-  //QUOTES 
-  getQuote(callWhenDone) {
-    console.log('looking for some good quotes')
-    quoteApi().then((res) => {
-      callWhenDone(res.data)
-    })
+  constructor() {
+    if (store) {
+      return store
+    }
+    store = this
   }
-
-  //TODO
-  getTodos(draw) {
-    console.log("Getting the Todo List")
-    todoApi.get('')
-      .then((res) => { // <-- WHY IS THIS IMPORTANT????
-
-      })
-      .catch(logError)
+  addSubscriber(dataName, fn) {
+    _state.Subscribers[dataName].push(fn)
   }
-
-  addTodo(todo) {
-    // WHAT IS THIS FOR???
-    todoApi.post('', todo)
-      .then(function (res) { // <-- WHAT DO YOU DO AFTER CREATING A NEW TODO?
-
-      })
-      .catch(logError)
-  }
-
-  toggleTodoStatus(todoId) {
-    // MAKE SURE WE THINK THIS ONE THROUGH
-    //STEP 1: Find the todo by its index **HINT** todoList
-
-    var todo = {} ///MODIFY THIS LINE
-
-    //STEP 2: Change the completed flag to the opposite of what is is **HINT** todo.completed = !todo.completed
-    todoApi.put(todoId, todo)
-      .then(function (res) {
-        //DO YOU WANT TO DO ANYTHING WITH THIS?
-      })
-      .catch(logError)
-  }
-
-  removeTodo() {
-    // Umm this one is on you to write.... The method is a DELETE
-
-  }
-
-  //WEATHER
-  getWeather(callWhenDone) {
-    console.log('Calling the Weatherman')
-    weatherApi().then(function (res) {
-      // HEY FUN FACT 
-      // Have you ever wanted to know the temperature measured in kelvin? That is what this data returns!
-      // res.data.temp is the temperature in Kelvin
-      // You should probably convert the temperature data to either F or C
-      callWhenDone(res.data);
-    })
+  setState(prop, value) {
+    _state[prop] = value
+    _subscribers.forEach(fn => fn());
   }
 
 }
